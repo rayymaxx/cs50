8a1df19472c46b0dd625a2277e08b519ac9120a6
rayymaxx <raymax461@gmail.com>
2025-03-06 10:52:24 -0800

    Finished lecture 2 on python

---
+++ dictionaries.py
@@ -0,0 +1,5 @@
+houses = {"Harry": "Gryffindor", "Draco": "Slytherin"}
+
+houses["Hermoine"] = "Gryffindor"
+
+print(houses["Hermoine"])
---
+++ lists.py
@@ -0,0 +1,10 @@
+# Define a list of names
+names = ["Harry", "Ron", "Hermoine", "Ginny"]
+
+print(names[0])
+
+names.append("Draco")
+
+names.sort()
+
+print(names)
